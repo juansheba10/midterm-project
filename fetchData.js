08414a2938e2717f3b96b63c5b5fac0efa60d757
Juan Sóto <juansheba97@gmail.com>
2022-09-23 14:17:54 +0200

    try

---
+++ fetchData.js
@@ -0,0 +1,79 @@
+const fetchData = "https://jsonplaceholder.typicode.com/posts/?_limit=3";
+
+async function getData() {
+  await fetch(fetchData)
+    .then((data) => data.json())
+    .then((cleanData) => {
+      let rawData = "";
+      cleanData.forEach((post) => {
+        rawData += `
+            <div class="card" style="width: 20rem">
+                <img src="/project-assets/projects-section/${
+                  post.id
+                }.jpg" class="card-img-top" alt="projects-3" />
+                <div class="card-body bg-white">
+                    <h5 class="card-title bg-white">${post.title.slice(
+                      0,
+                      15
+                    )}</h5>
+                    <p class="card-text bg-white">
+                        ${post.body.slice(0, 12)}
+                    </p>
+                    <a href="#" class="btn text-primary">Go somewhere</a>
+                </div>
+            </div>
+              `;
+      });
+      document.querySelector(".card-deck").innerHTML = rawData;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+getData();
+
+const projectRawData = "https://jsonplaceholder.typicode.com/posts/?_limit=1";
+
+async function projectData() {
+  await fetch(projectRawData)
+    .then((data) => data.json())
+    .then((cleanData) => {
+      let rawData = "";
+      cleanData.forEach((post) => {
+        rawData += `
+      <h1 class="project-heading">${post.title.slice(0, 15)} </h1>
+      <div class="project-type">
+        <div class="project-page-type">${post.body.slice(0, 15)}</div>
+        <div class="project-date">
+          <div class="project-completed"> Completed on</div>
+          <div class="project-date">2/2/22
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="project-image-contain">
+      <img class="project-image" src="/project-assets/projects-section/${
+        post.id
+      }.jpg" alt=""
+        sizes="(max-width: 479px)88vw (max-width 767px) 92vw, 94vw">
+      <img src="/project-assets/projects-section/1.jpg" alt="" class="project-image-effect"
+        sizes="(max-width: 479px)88vw (max-width 767px) 92vw, 94vw">
+
+    </div>
+    <div class=" project-detail">
+      <p>${post.body}. ${post.body}. ${post.body}.</p>
+    </div>
+  </div>
+  </div>
+        `;
+      });
+      document.querySelector(".section-project").innerHTML = rawData;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+projectData();
--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+// MENU HAMBURGUESA RESPONSIVE
+
 const hamburger = document.getElementsByClassName("hamburger")[0];
 const navMenu = document.getElementsByClassName("nav-menu")[0];
 
 hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+document.querySelectorAll(".nav-item").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    hamburger.classList.remove("active");
+  })
+);
+
+// CONTACT FORM
 
 document.querySelector("#email-form").addEventListener("submit", addPost);
 
@@ -13,10 +25,10 @@ function addPost(preventForm) {
   preventForm.preventDefault();
 
   // Apuntar a todos los inputs que necesitamos
-  let name = document.querySelector("name").value;
-  let email = document.querySelector("email").value;
-  let phone = document.querySelector("phone").value;
-  let message = document.querySelector("message").value;
+  let name = document.querySelector("#name").value;
+  let email = document.querySelector("#name-2").value;
+  let phone = document.querySelector("#name-3").value;
+  let message = document.querySelector("#field").value;
 
   // vamos a usar el metodo fetch, llamamos a la constante url que esta afuera y le pasamos el objeto con 3 propiedades[method, headers, body]
 
